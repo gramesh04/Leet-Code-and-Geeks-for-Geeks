e19561b65ae9fa82df0c13bd837eff5f81723021
G RAMESH <143642230+GANDEEDRAMESH@users.noreply.github.com>
2026-01-31 09:59:11 +0530

    Time: 4 ms (79.92%), Space: 46.6 MB (30.82%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        } else {
+            char[] schar = s.toCharArray();
+            char[] tchar = t.toCharArray();
+            Arrays.sort(schar);
+            Arrays.sort(tchar);
+            return Arrays.equals(schar, tchar);
+
+        }
+    }
+}
