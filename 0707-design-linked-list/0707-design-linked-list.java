936ecece7d02334ec9699744305c4c712707cdee
G RAMESH <143642230+GANDEEDRAMESH@users.noreply.github.com>
2026-02-05 09:40:08 +0530

    Time: 6 ms (99.89%), Space: 47.1 MB (31.88%) - LeetHub

---
+++ 0707-design-linked-list/0707-design-linked-list.java
@@ -0,0 +1,87 @@
+class MyLinkedList {
+    class Node {
+        int val;
+        Node next, prev;
+        Node(int val) { this.val = val; }
+    }
+
+    private int size;
+    private Node head, tail;
+
+    public MyLinkedList() {
+        size = 0;
+        head = new Node(0);
+        tail = new Node(0);
+        head.next = tail;
+        tail.prev = head;
+    }
+
+    public int get(int index) {
+        if (index < 0 || index >= size) return -1;
+        Node curr = head;
+        if (index + 1 < size - index) {
+            for (int i = 0; i <= index; i++) curr = curr.next;
+        } else {
+            curr = tail;
+            for (int i = 0; i < size - index; i++) curr = curr.prev;
+        }
+        return curr.val;
+    }
+
+    public void addAtHead(int val) {
+        addAtIndex(0, val);
+    }
+
+    public void addAtTail(int val) {
+        addAtIndex(size, val);
+    }
+
+    public void addAtIndex(int index, int val) {
+        if (index > size) return;
+        if (index < 0) index = 0;
+        Node pred, succ;
+        if (index < size - index) {
+            pred = head;
+            for (int i = 0; i < index; i++) pred = pred.next;
+            succ = pred.next;
+        } else {
+            succ = tail;
+            for (int i = 0; i < size - index; i++) succ = succ.prev;
+            pred = succ.prev;
+        }
+        size++;
+        Node toAdd = new Node(val);
+        toAdd.prev = pred;
+        toAdd.next = succ;
+        pred.next = toAdd;
+        succ.prev = toAdd;
+    }
+
+    public void deleteAtIndex(int index) {
+        if (index < 0 || index >= size) return;
+        Node pred, succ;
+        if (index < size - index) {
+            pred = head;
+            for (int i = 0; i < index; i++) pred = pred.next;
+            succ = pred.next.next;
+        } else {
+            succ = tail;
+            for (int i = 0; i < size - index - 1; i++) succ = succ.prev;
+            pred = succ.prev.prev;
+        }
+        size--;
+        pred.next = succ;
+        succ.prev = pred;
+    }
+}
+
+
+/**
+ * Your MyLinkedList object will be instantiated and called as such:
+ * MyLinkedList obj = new MyLinkedList();
+ * int param_1 = obj.get(index);
+ * obj.addAtHead(val);
+ * obj.addAtTail(val);
+ * obj.addAtIndex(index,val);
+ * obj.deleteAtIndex(index);
+ */
