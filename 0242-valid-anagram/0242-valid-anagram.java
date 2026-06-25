0d317e7c93ee9b35e7c10123c6d0fd16c83e1770
G RAMESH <143642230+gramesh04@users.noreply.github.com>
2026-06-25 12:50:08 +0530

    Time: 4 ms (82.14%), Space: 46.3 MB (46.06%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,14 +1,15 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if (s.length() != t.length()) {
-            return false;
-        } else {
-            char[] schar = s.toCharArray();
-            char[] tchar = t.toCharArray();
-            Arrays.sort(schar);
-            Arrays.sort(tchar);
-            return Arrays.equals(schar, tchar);
-
-        }
+       if(s.length() != t.length()){
+        return false;
+       
+       } 
+       else{
+        char[] schar = s.toCharArray();
+        char[] tchar = t.toCharArray();
+        Arrays.sort(schar);
+        Arrays.sort(tchar);
+        return Arrays.equals(tchar,schar);
+       }
     }
 }
